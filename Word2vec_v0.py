42f28da964f4ff3f745103f9cbbd50968191c445
guillermo-andres <guillermo.betancourt@upr.edu>
2019-10-17 18:38:50 -0400

    w2v trili, lets get it

--- Word2vec_v0.py
+++ Word2vec_v0.py
@@ -4,3 +4,15 @@ from nltk.corpus import brown
 
 
 vocab_v0 = parse_Questions(brown)
+
+train_data = vocab_v0.fromkeys
+
+embedding_dim=16
+
+model = keras.Sequential([
+  layers.Embedding(95000, embedding_dim),
+  layers.GlobalAveragePooling1D(),
+  layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()

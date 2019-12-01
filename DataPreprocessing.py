83ad0e65d8666559782f3bb459fd3c5e518a163c
guillermo-andres <guillermo.betancourt@upr.edu>
2019-12-01 04:47:06 -0400

    Veryfing model adaptability to treebank corpus and trying to find the cap in acc. with curent model arquitecture

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -65,11 +65,12 @@ class DoublyLinkedList:
 class Pre:
 
     @staticmethod
-    def parse_Questions(corpus):
-        count = 0
+    def parse_Questions(corpus, c):
+        count = c
         for sentence in corpus.sents():
             vocab_v0[count] = sentence
             count = count + 1
+        return count
 
     # Method that receives list of tagged sentences and iterates through them to pass them into a dictionary.
     # Brown_mapping and tree_mapping equals the map of sentences:tags
@@ -96,8 +97,8 @@ class Pre:
     # Function that takes the labels variables with integers and iterates through  to
     # hot encode them and return the transformed data
     @staticmethod
-    def labelWordEncoder():
-        all_tags = set([tag for sentence in brown.tagged_sents(tagset='universal') for _, tag in sentence])
+    def labelWordEncoder(corpus):
+        all_tags = set([tag for sentence in corpus.tagged_sents(tagset='universal') for _, tag in sentence])
         one_hot_labels = to_categorical([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], num_classes=12)
         # Iterate through all the tags to do an int mapping
         c = 0
@@ -122,44 +123,56 @@ class Pre:
 # /////////////////////////////////////////////////////////////////////////////////////////////////
 # //////////////////////////////////////////////MAIN//////////////////////////////////////////////
 
-nltk.download('brown')
-nltk.download('punkt')
-nltk.download('universal_tagset')
+
 one_hot_labels = None
 vocab_v0 = {}
 wordVocab = {}
 tag_mapping = {}
-Pre.parse_Questions(brown)
+count_until = Pre.parse_Questions(treebank,0)
+Pre.parse_Questions(brown,count_until)
 Pre.labelWordEncoder()
-brown_tagsents = dllist(brown.tagged_sents(tagset='universal'))
+brown_tagsents = dllist(treebank.tagged_sents(tagset='universal'))
 xTrain, yTrain = Pre.dataSegmentation(brown_tagsents)
 
 ############################################              MODEL                    ########################################################################
 # Data Partition
 # Padding Vectorized Data Set
-x_train = tf.keras.preprocessing.sequence.pad_sequences(xTrain[:40000], padding='post')
-y_train = tf.keras.preprocessing.sequence.pad_sequences(yTrain[:40000], padding='post')
+x_train = tf.keras.preprocessing.sequence.pad_sequences(xTrain[:3914], padding='post')
+y_train = tf.keras.preprocessing.sequence.pad_sequences(yTrain[:3914], padding='post')
 
-x_validation = x_train[30000:]
-y_validation = y_train[30000:]
-x_train = x_train[:30000]
-y_train = y_train[:30000]
+x_validation = x_train[3000:]
+y_validation = y_train[3000:]
+x_train = x_train[:3000]
+y_train = y_train[:3000]
 
-print(y_train.shape, x_train.shape)
+
 vocab_size = len(wordVocab.keys())
 model = models.Sequential()
-model.add(layers.Embedding(input_dim=vocab_size+1, input_length=180, output_dim=100, mask_zero=True))
-model.add(layers.Dense(100, activation='relu'))
+model.add(layers.Embedding(input_dim=vocab_size, input_length=271, output_dim=200, mask_zero=True))
+model.add(layers.Dense(200, activation='relu'))
+model.add(layers.Dense(100,activation='relu'))
 model.add(layers.Dense(12, activation='softmax', ))
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 history = model.fit(x_train, y_train, epochs=20, batch_size=512,
                     validation_data=(x_validation, y_validation))
 
+
+epochs = range(1, len(loss) + 1)
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+acc = history.history['acc']
+val_acc = history.history['val_acc']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-epochs = range(1, len(loss) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
 plt.plot(epochs, loss, 'bo', label='Training Loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')

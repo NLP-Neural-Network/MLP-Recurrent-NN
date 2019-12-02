60faf6e40a28260cb9ac80736ce3a18ec41660c8
guillermo-andres <guillermo.betancourt@upr.edu>
2019-12-02 15:40:19 -0400

    added premature gpu support, must test

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -1,23 +1,23 @@
-
-    from llist import dllist
-    import nltk
-    from nltk.corpus import brown, treebank
-    from collections import defaultdict, Counter
-    from gensim.models.word2vec import Word2Vec
-    from multiprocessing import cpu_count
-    import gensim.downloader as api
-    import tensorflow as tf
-    import numpy as np
-    from tensorflow.keras.utils import to_categorical
-    from tensorflow.keras import models, layers
-    from tensorflow.keras.layers import LSTM, Dense, Embedding, Input, Reshape
-    import matplotlib.pyplot as plt
-    from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
+import nltk
+from nltk.corpus import brown, treebank
+from collections import defaultdict, Counter
+from gensim.models.word2vec import Word2Vec
+from multiprocessing import cpu_count
+import gensim.downloader as api
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import models, layers
+from tensorflow.keras.layers import LSTM, Dense, Embedding, Input, Reshape
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
 
 
     # /////////////////////////////////Clases////////////////////////////////////////////////////////////////
     # ///////////////////////////////////////////////////////////////////////////////////////////////////////
-    class Pre:
+class Pre:
+        sentences = []
+        sentence_tags = []
 
         @staticmethod
         def parse_Questions(corpus, c):
@@ -27,36 +27,38 @@
                 count = count + 1
             return count
 
-        # Method that receives list of tagged sentences and iterates through them to pass them into a dictionary.
-        # Brown_mapping and tree_mapping equals the map of sentences:tags
+        # Method that receives list of tagged sentences and iterates through them to build word set and tag set.
         @staticmethod
-        def dataSegmentation(tagged_corpus):
-            sentences, sentence_tags =[], []
+        def vocabularyBuilder(tagged_corpus):
+
             for tagged_sentence in tagged_corpus:
                 sentence, tags = zip(*tagged_sentence)
-                sentences.append(np.array(sentence))
-                sentence_tags.append(np.array(tags))
+                Pre.sentences.append(np.array(sentence))
+                Pre.sentence_tags.append(np.array(tags))
 
             words, tags = set([]), set([])
 
-            for s in sentences:
+            for s in Pre.sentences:
                 for w in s:
                     words.add(w.lower())
 
-            for ts in sentence_tags:
+            for ts in Pre.sentence_tags:
                 for t in ts:
                     tags.add(t)
 
-            word2index = {w: i  for i, w in enumerate(list(words))}
-            #word2index['-PAD-'] = 0  # The special value used for padding
-            #word2index['-OOV-'] = 0  # The special value used for OOVs
+            word2index = {w: i + 2 for i, w in enumerate(list(words))}
+            word2index['-PAD-'] = 0  # The special value used for padding
+            word2index['-OOV-'] = 0  # The special value used for OOVs
             tag2index = {t: i for i, t in enumerate(list(tags))}
-            #tag2index['-PAD-'] = 0  # The special value used to padding
 
-            tag2index
+            return word2index,tag2index
+
+        #Method that segments the data into 2 matrices each with a value mapping to an individual word in the x matrix and its corresponding universal tag in y
+        @staticmethod
+        def dataSegmentation(word2index,tag2index):
             train_sentences_X, train_tags_y = [], []
 
-            for s in sentences:
+            for s in Pre.sentences:
                 s_int = []
                 for w in s:
                     try:
@@ -66,7 +68,7 @@
 
                 train_sentences_X.append(s_int)
 
-            for s in sentence_tags:
+            for s in Pre.sentence_tags:
                 train_tags_y.append([tag2index[t] for t in s])
 
             return np.asarray(train_sentences_X), np.asarray(train_tags_y)
@@ -102,37 +104,32 @@
     # //////////////////////////////////////////////MAIN//////////////////////////////////////////////
 
 
-    one_hot_labels = None
-    vocab_v0 = {}
-    wordVocab = {}
-    tag_mapping = {}
-    count_until = Pre.parse_Questions(brown,0)
+one_hot_labels = None
+vocab_v0, tag_mapping  = Pre.vocabularyBuilder(brown.tagged_sents(tagset='universal'))
+wordVocab = {}
+Pre.parse_Questions(brown,0)
 
-    Pre.labelWordEncoder(brown)
-
-    brown_tagsents = brown.tagged_sents(tagset='universal')
-    xTrain, yTrain = Pre.dataSegmentation(brown_tagsents)
+brown_tagsents = brown.tagged_sents(tagset='universal')
+xTrain, yTrain = Pre.dataSegmentation(vocab_v0,tag_mapping)
 
     ############################################              MODEL                    ########################################################################
     # Data Partition
     # Padding Vectorized Data Set
-    x_data = tf.keras.preprocessing.sequence.pad_sequences(xTrain, padding='post')
-    y_data = tf.keras.preprocessing.sequence.pad_sequences(yTrain, padding='post')
+x_data = tf.keras.preprocessing.sequence.pad_sequences(xTrain, padding='post')
+y_data = tf.keras.preprocessing.sequence.pad_sequences(yTrain, padding='post')
 
-    x_training = x_data[:50000]
-    y_training = y_data[:50000]
+x_training = x_data[:50000]
+y_training = y_data[:50000]
 
-    x_validation = x_data[50000:]
-    y_validation = y_data[50000:]
-    x_training.shape
-    lol = to_categorical(y_training,)
-    lol[0]
-
-    vocab_size = len(wordVocab.keys())
+x_validation = x_data[50000:]
+y_validation = y_data[50000:]
 
 
+vocab_size = len(vocab_v0)
 
 
+mirrored_strategy = tf.distribute.MirroredStrategy()
+with mirrored_strategy.scope():
     model = models.Sequential()
     model.add(layers.InputLayer(input_shape=(180, )))
     model.add(layers.Embedding(input_dim=vocab_size, input_length=180, output_dim=200, mask_zero=True))
@@ -140,38 +137,38 @@
     model.add(layers.TimeDistributed(Dense(12)))
     model.add(layers.Activation('softmax'))
     model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+                optimizer='adam',
+                metrics=['accuracy'])
 
 
-    history =  model.fit(x_training,to_categorical(y_training), batch_size=128, epochs=40, validation_split=.2)
+    history =  model.fit(x_training,to_categorical(y_training), batch_size=512, epochs=40)
 
 
 
 
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-    epochs = range(1, len(loss) + 1)
+epochs = range(1, len(loss) + 1)
 
 
-    plt.plot(epochs, acc, 'bo', label='Training acc')
-    plt.plot(epochs, val_acc, 'b', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-    plt.figure()
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
 
-    plt.plot(epochs, loss, 'bo', label='Training Loss')
-    plt.plot(epochs, val_loss, 'b', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
+plt.plot(epochs, loss, 'bo', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
 
-    plt.show()
+plt.show()
 
-    plt.show()
+plt.show()
